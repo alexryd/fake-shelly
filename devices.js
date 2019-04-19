d0cdf1c7facfacfb53df0f98092232adfa70a12c
Alexander Ryden <alexander.ryden@gmail.com>
2019-04-19 11:08:24 +0200

    Add Shelly H&T

--- devices.js
+++ devices.js
@@ -245,6 +245,17 @@ class Shelly2 extends Device {
   }
 }
 
+class ShellyHT extends Device {
+  constructor(id, host) {
+    super('SHHT-1', id, host)
+
+    this._defineProperty('temperature', 33, 25.8, Number)
+    this._defineProperty('humidity', 44, 151, Number)
+    this._defineProperty('battery', 77, 97, Number)
+  }
+}
+
 module.exports = {
   Shelly2,
+  ShellyHT,
 }
