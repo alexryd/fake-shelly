70d00dda66eaf6fe5eeaa73c81b393e9e11152c8
Alexander Ryden <alexander.ryden@gmail.com>
2019-04-27 18:01:29 +0200

    Create a new main package

---
+++ index.js
@@ -0,0 +1,9 @@
+const CoapServer = require('./coap-server')
+const Devices = require('./devices')
+const HttpServer = require('./http-server')
+
+module.exports = {
+  CoapServer,
+  Devices,
+  HttpServer,
+}
